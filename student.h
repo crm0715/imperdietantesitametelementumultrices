2a1dfd222f6dea09aa8fa77e5f04682b470a30dc
Natalie <natalie@Ren.(none)>
2011-12-02 14:44:49 -0500

    updated flavours

--- student.h
+++ student.h
@@ -3,7 +3,16 @@
 
 #include <uC++.h>
 
+#include "printer.h"
+#include "nameServer.h"
+#include "watCardOffice.h"
+
 _Task Student {
+	Printer &prt;
+	NameServer &nameServer;
+	WATCardOffice &cardOffice;
+	unsigned int id;
+	unsigned int maxPurchases;
     void main();
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,

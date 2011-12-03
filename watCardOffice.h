51028f480fb066d73ac5ac62a52aef1ba937387f
Bill Cao <caoruiming123@gmail.com>
2011-12-03 18:23:30 -0500

    finishing up office

--- watCardOffice.h
+++ watCardOffice.h
@@ -2,25 +2,50 @@
 #define __WATCARDOFFICE_H
 
 #include <uC++.h>
+#include <vector>
+#include <list>
 #include "watCard.h"
 #include "bank.h"
 #include "printer.h"
 
 _Task WATCardOffice {
     struct Args {
-
+        unsigned int sid;
+        unsigned int amount;
+        WATCard *card;
     };
     struct Job {				// marshalled arguments and return future
-        Args args;				// call arguments (YOU DEFINE "Args")
+        Args args;				// call arguments
         FWATCard result;			// return future
         Job( Args args ) : args( args ) {}
     };
-    _Task Courier { };			// communicates with bank
 
+    _Task Courier {
+        Printer &prt;
+        Bank &bank;
+        WATCardOffice &cardOffice;
+        unsigned int id;
+
+        void main();
+      public:
+        Courier(Printer &prt, Bank &bank, WATCardOffice &cardOffice, unsigned int id);
+    };
+
+    _Mutex void createJob(); // used by server to create jobs for courier
+    FWATCard transferHelper(unsigned int sid, unsigned int amount, WATCard *card); // used by client to indicate transfer of money is needed
+    std::list<Job *> jobs;
+    std::vector<Courier *> couriers;
+
+    // for communication
+    Args previousArgs;
+    FWATCard previousFWATCard;
+
+    Printer &prt;
     void main();
   public:
     _Event Lost {};
     WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
+    ~WATCardOffice();
     FWATCard create( unsigned int sid, unsigned int amount, WATCard *&card );
     FWATCard transfer( unsigned int sid, unsigned int amount, WATCard *card );
     Job *requestWork();

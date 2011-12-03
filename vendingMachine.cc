d46f091a0bdf112ec04c0932b39de6b6d76d1e5d
Bill Cao <caoruiming123@gmail.com>
2011-12-02 22:39:44 -0500

    Finished Vending Machine

--- vendingMachine.cc
+++ vendingMachine.cc
@@ -1,22 +1,53 @@
 #include "vendingMachine.h"
 
-VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost, unsigned int maxStockPerFlavour ) {
+VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost, unsigned int maxStockPerFlavour ) :
+    prt(prt),
+    nameServer(nameServer),
+    id(id),
+    sodaCost(sodaCost),
+    maxStockPerFlavour(maxStockPerFlavour)
+{
+    nameServer.VMregister(this);
 } //VendingMachine::VendingMachine
 
-void VendingMachine::main() {
-} //VendingMachine::main
-
 VendingMachine::Status VendingMachine::buy( Flavours flavour, WATCard &card ) {
+  if (card.getBalance() < sodaCost) return FUNDS;
+  if (stock[flavour] < 1) return STOCK;
+    flavourBought = flavour;
+    card.withdraw(sodaCost);
+    return BUY;
 } //VendingMachine::buy
 
 unsigned int *VendingMachine::inventory() {
+    return stock;
 } //VendingMachine::inventory
 
 void VendingMachine::restocked() {
+
 } //VendingMachine::restocked
 
 _Nomutex unsigned int VendingMachine::cost() {
+    return sodaCost;
 } //VendingMachine::cost
 
 _Nomutex unsigned int VendingMachine::getId() {
+    return id;
 } //VendingMachine::getId
+
+void VendingMachine::main() {
+    prt.print(Printer::Vending, id, 'S');
+    for ( ;; ) {
+        _Accept(~VendingMachine) {
+            break;
+        } or _Accept(inventory) {
+            prt.print(Printer::Vending, id, 'r');
+            _Accept(restocked) {
+                prt.print(Printer::Vending, id, 'R');
+            } // _Accept
+        } or _Accept(buy) {
+            stock[flavourBought]--;
+            prt.print(Printer::Vending, id, 'B', (int)flavourBought, stock[flavourBought]);
+        } //_Accept
+    } // for
+    prt.print(Printer::Vending, id, 'F');
+} //VendingMachine::main
--- vendingMachine.h
+++ vendingMachine.h
@@ -7,9 +7,20 @@
 #include "nameServer.h"
 
 _Task VendingMachine {
-    void main();
   public:
     enum Flavours { RABBIT, CAT, DOG, BEAR, NUM_OF_FLAVOURS }; 			// flavours of soda (YOU DEFINE)
+  private:
+    Printer &prt;
+    NameServer &nameServer;
+    unsigned int id;
+    unsigned int sodaCost;
+    unsigned int maxStockPerFlavour;
+    bool restockingFlag;
+    unsigned int stock[NUM_OF_FLAVOURS];
+    Flavours flavourBought;
+    WATCard *cardUsed;
+    void main();
+  public:
     enum Status { BUY, STOCK, FUNDS };		// purchase status: successful buy, out of stock, insufficient funds
     VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost, unsigned int maxStockPerFlavour );
     Status buy( Flavours flavour, WATCard &card );
